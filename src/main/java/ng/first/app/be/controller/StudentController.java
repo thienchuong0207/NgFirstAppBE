c352b64705daaf7cc2aad38ad58a21a9799761cf
Chuong Ton <thienchuong0207@gmail.com>
2018-10-20 14:36:57 -0600

    Update CORS Configuration

--- src/main/java/ng/first/app/be/controller/StudentController.java
+++ src/main/java/ng/first/app/be/controller/StudentController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import ng.first.app.be.constant.StudentConstant;
 import ng.first.app.be.dto.StudentDTO;
