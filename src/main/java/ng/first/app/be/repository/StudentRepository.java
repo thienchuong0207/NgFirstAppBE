bdb8f0070125574f931506acea4722d628df6a67
CORP\chuong.ton <chuong.ton@jeppesen.com>
2018-11-12 10:46:13 -0700

    Refactor Code

--- src/main/java/ng/first/app/be/repository/StudentRepository.java
+++ src/main/java/ng/first/app/be/repository/StudentRepository.java
@@ -1,7 +1,7 @@
 package ng.first.app.be.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,9 +9,6 @@ import org.springframework.stereotype.Repository;
 
 import ng.first.app.be.constant.StudentConstant;
 import ng.first.app.be.entity.StudentEntity;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 /**
  * Student Repository
